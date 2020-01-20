2d3b93173a308902875a7d8558bb261ab9cd3065
Dmitriy <k.d.s.98@mail.ru>
2020-01-20 15:25:50 +0300

    running bash scripts template update

--- experiments/beam_width/run_model_on_beam_grid.sh
+++ experiments/beam_width/run_model_on_beam_grid.sh
@@ -1,16 +1,32 @@
+GPU_DEVICE_ID=3
+
+DATA_PATH="data-bin/wmt14.en-fr.newstest2014"
+
+MODEL_PATH="wmt14.en-fr.fconv-py"
+CHECKPOINT_PATH="model.pt"
+SCORES_PATH="scores_regless"
+
 for i in {1..9}
 do
-	echo $i
-	CUDA_VISIBLE_DEVICES=3 fairseq-generate data-bin/iwslt14.tokenized.de-en \
-    		--path mtransformer/checkpoints/checkpoint_best.pt \
-    		--batch-size 64 --beam $i --remove-bpe --diverse-beam-strength 0 > mtransformer/scores_regless/w0$i/gen.out
+	OUT_PATH="${MODEL_PATH}/${SCORES_PATH}/w0${i}"
+	echo $OUT_PATH
+	if [ ! -d ./$OUT_PATH ]; then
+		mkdir ./$OUT_PATH
+	fi
+	CUDA_VISIBLE_DEVICES=$GPU_DEVICE_ID fairseq-generate ./$DATA_PATH \
+    		--path ./$MODEL_PATH/$CHECKPOINT_PATH \
+    		--batch-size 64 --beam $i --remove-bpe --diverse-beam-strength 0 > ./$OUT_PATH/gen.out
 done
 
 for i in {10..50..10}
 do
-	echo $i
-	CUDA_VISIBLE_DEVICES=3 fairseq-generate data-bin/iwslt14.tokenized.de-en \
-    		--path mtransformer/checkpoints/checkpoint_best.pt \
-    		--batch-size 16 --beam $i --remove-bpe --diverse-beam-strength 0 > mtransformer/scores_regless/w$i/gen.out
+	OUT_PATH="${MODEL_PATH}/${SCORES_PATH}/w${i}"
+	echo $OUT_PATH
+	if [ ! -d ./$OUT_PATH ]; then
+		mkdir ./$OUT_PATH
+	fi
+	CUDA_VISIBLE_DEVICES=$GPU_DEVICE_ID fairseq-generate ./$DATA_PATH \
+    		--path ./$MODEL_PATH/$CHECKPOINT_PATH \
+    		--batch-size 16 --beam $i --remove-bpe --diverse-beam-strength 0 > ./$OUT_PATH/gen.out
 done
 
--- experiments/beam_width/run_score_calc.sh
+++ experiments/beam_width/run_score_calc.sh
@@ -1,8 +1,13 @@
+GPU_DEVICE_ID=3
+
+MODEL_DIR="wmt14.en-fr.fconv-py"
 SCORE_DIR="scores_regless"
-for dir in ./$SCORE_DIR/*
+
+for dir in ./$MODEL_DIR/$SCORE_DIR/*
 do
     grep ^H $dir/gen.out | cut -f3- > $dir/gen.out.sys
     grep ^T $dir/gen.out | cut -f2- > $dir/gen.out.ref
     grep ^S $dir/gen.out | cut -f2- > $dir/gen.out.in
-    CUDA_VISIBLE_DEVICES=3 fairseq-score --sys $dir/gen.out.sys --ref $dir/gen.out.ref --sentence-bleu > $dir/score.out
+    CUDA_VISIBLE_DEVICES=$GPU_DEVICE_ID fairseq-score --sys $dir/gen.out.sys --ref $dir/gen.out.ref --sentence-bleu --diverse-beam-strength 0 > $dir/score.out
+    CUDA_VISIBLE_DEVICES=$GPU_DEVICE_ID fairseq-score --sys $dir/gen.out.sys --ref $dir/gen.out.ref --diverse-beam-strength 0 > $dir/total.out
 done
